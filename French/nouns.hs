f4d0244a9ffaeb281eba94c5fb9103867e1b6510
Grégoire Détrez <gdetrez@crans.org>
2011-10-11 04:37:21 +0200

    Updating experiment in French to reflect the change in the library

---
+++ French/nouns.hs
@@ -0,0 +1,31 @@
+module Main where
+
+import GF.Predictability
+import GF.Predictability.Utils
+
+-- ************************************ NOUNS ********************************
+testNoun :: TestFunction
+testNoun = (==)
+
+setupNoun :: SetupFunction
+setupNoun ("":_) = skip $ "No singular form"
+setupNoun (_:"":_) = skip $ "No plural form"
+setupNoun [oeuil, yeux, gender] = do
+  gender' <- case gender of
+    "CommonRomance.Fem"  -> return "feminine"
+    "CommonRomance.Masc" -> return "masculine"
+    _ -> skip $ "invalid gender string: " ++ gender
+  return
+    [ [esc oeuil]
+    , [esc oeuil, gender' ]
+    , [esc oeuil, esc yeux, gender' ] ]
+setupNoun o = skip $ "Invalid lexicon entry"
+ 
+main = do
+  let nounEx = mkExperimentWithFunctions 
+               "Nouns" 
+               "alltenses/ParadigmsFre.gfo"
+               "mkN"
+               setupNoun
+               testNoun
+  mainRunExperiment nounEx
---
+++ French/verbs.hs
@@ -0,0 +1,44 @@
+module Main where
+
+import GF.Predictability
+import GF.Predictability.Utils
+
+-- ********************************** VERBS **********************************
+testVerb :: TestFunction
+testVerb l1 l2 = and $ zipWith (==) l1 (head l2:l2)
+
+setupVerb :: SetupFunction
+setupVerb vForms | [] `elem` vForms = skip $ "Missing form"
+setupVerb vForms | length vForms /= 51 = skip "Incorrect number of forms in lexicon"
+setupVerb vForms = do
+  let jeter = vForms!!0
+      jette = vForms!!3
+      jettera = vForms!!33
+      tenir = vForms!!0
+      tiens = vForms!!1
+      tient = vForms!!3
+      tenons = vForms!!4
+      tenez = vForms!!5
+      tiennent = vForms!!6
+      tienne = vForms!!7
+      tenions = vForms!!10
+      tiensI = vForms!!43
+      tint = vForms!!27
+      tiendra = vForms!!33
+      tenu = vForms!!46
+  return $ map (map esc )
+    [ [ jeter ]
+    , [ jeter, jette, jettera ]
+    , [ tenir, tiens, tenons, tiennent, tint, tiendra, tenu ]
+    , [ tenir,tiens,tient,tenons,tenez,tiennent,tienne,tenions,tiensI,tint,tiendra,tenu ]
+    ]
+ 
+main = do
+  let verbEx = mkExperimentWithFunctions 
+               "French Verbs" 
+               "alltenses/ParadigmsFre.gfo"
+               "mkV"
+               setupVerb
+               testVerb
+  
+  mainRunExperiment verbEx
