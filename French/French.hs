abcba2840639ef3d0afdf5f1ea2f932134ca46c6
Grégoire Détrez <gdetrez@crans.org>
2011-10-11 04:37:07 +0200

    Updating experiment in French to reflect the change in the library

--- French/French.hs
+++ French/French.hs
@@ -29,7 +29,8 @@ setupAdj :: SetupFunction
 setupAdj fs@[beau,beaux,belle,belles] | "" `elem` fs = skip $ "Missing form"
 setupAdj [beau,beaux,belle,belles] = return
     [ [esc beau]
-    , [esc beau, esc belle ] ]
+    , [esc beau, esc belle ]
+    , [esc beau, esc belle, esc belles ] ]
 --    , [esc beau, esc belle, esc beaux ] ]
 setupAdj o = skip $ "Invalid lexicon entry: " ++ show o
 
@@ -43,9 +44,25 @@ setupVerb vForms | length vForms /= 51 = skip "Incorrect number of forms in lexi
 setupVerb vForms = do
   let jeter = vForms!!0
       jette = vForms!!2
-      jettera = vForms!!31
-  return [ [ esc jeter ]
-         , [ esc jeter, esc jette, esc jettera ] ]
+      jettera = vForms!!33
+      tenir = vForms!!0
+      tiens = vForms!!1
+      tient = vForms!!3
+      tenons = vForms!!4
+      tenez = vForms!!5
+      tiennent = vForms!!6
+      tienne = vForms!!7
+      tenions = vForms!!10
+      tiensI = vForms!!43
+      tint = vForms!!27
+      tiendra = vForms!!33
+      tenu = vForms!!46
+  return $ map (map esc )
+    [ [ jeter ]
+    , [ jeter, jette, jettera ]
+    , [ tenir, tiens, tenons, tiennent, tint, tiendra, tenu ]
+    , [ tenir,tiens,tient,tenons,tenez,tiennent,tienne,tenions,tiensI,tint,tiendra,tenu ]
+    ]
  
 main = do
   commonNouns <- readCSVLexicon "nouns.csv"
