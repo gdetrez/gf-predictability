5bb4afb22d477ffae40add7120922b8869028160
Grégoire Détrez <gdetrez@crans.org>
2011-10-27 17:31:50 +0200

    Adding the parallel verbs experiment in Swedish
    In this experiment, the one argument version of mkV is given the infinitive form instead of the present

--- Swedish/verbs_inf.hs
+++ Swedish/verbs_inf.hs
@@ -1,5 +1,5 @@
 module Main where
-
+{- This experiment is almost the same as the verbs experiment but for the one place mkV smart paradigm, instead of giving the infinitive, as it is traditionaly done, we give the present tense. It seems that this makes it easier to guess the correct paradigm, this experiment tries to confirm that. -}
 
 import GF.Predictability
 import GF.Predictability.Utils
@@ -23,7 +23,7 @@ setupVerb [ går,gås,gick,gicks,
             gå_imp,gå_inf,gås_inf,gått,gåtts,
             gången,gångens,gånget,gångets,
             gångna_undef,gångnas_undef,gångna_def,gångnas_def ] =
-  return [ [ esc gå_inf ]
+  return [ [ esc går ]
          , [ esc gå_inf, esc gick, esc gått ]
          , [ esc gå_inf, esc går, esc gå_imp, esc gick, esc gått, esc gången ] ]
 setupVerb o = skip $ "Invalid lexicon entry"
@@ -32,7 +32,7 @@ setupVerb o = skip $ "Invalid lexicon entry"
 main :: IO ()
 main = do
   let verbEx = mkExperimentWithFunctions 
-               "Swedish verbs (with infinitive)"
+               "Swedish verbs (with present)"
                "alltenses/ParadigmsSwe.gfo"
                "mkV"
                setupVerb
